0501fc856bdc7ec0814069005a38f6aa78e52bff
Laure Anne-Gethro Jean Bijou <laureanne-gbijou@Laures-MacBook-Air.local>
2024-11-21 19:55:28 -0500

    first commit

---
+++ Debug/src/CSC450-1-Week1.d
@@ -0,0 +1,1 @@
+src/CSC450-1-Week1.o: ../src/CSC450-1-Week1.cpp
---
+++ src/CSC450-1-Week1.cpp
@@ -0,0 +1,27 @@
+//============================================================================
+// Name        : CSC450-1-Week1.cpp
+// Author      : Laure A-G Jean
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Simple C++ Application
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	//Information about Bijou Girbson
+	string firstName = "Girbson";
+	string lastName = "Bijou";
+	string streetAddress = "Belvil";
+	string city = "Petion-Ville";
+	string zipCode = "6110H";
+
+	//Print out the information about Bijou Girbson
+	cout << "First Name: " << firstName << endl; // prints first name
+	cout << "Last Name: " << lastName << endl; // prints first name
+	cout << "Street Address: "<< streetAddress << endl; // prints first name
+	cout << "City: " << city << endl; // prints first name
+	cout << "Zip code: " << zipCode << endl; // prints first name
+	return 0;
+}
